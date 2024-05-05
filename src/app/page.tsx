1a3cfa224ef1afc3d11ce7a1c03e8ca7641164b3
mono300genuine <monolinker31@gmail.com>
2024-05-06 01:44:04 +0900

    bg update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Brain } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import AnimatedTextCharacter from "./components/AnimateTextWord";
 import { Disclaimer } from "./components/Disclaimer";
@@ -11,8 +10,21 @@ import { Button } from "./components/ui/button";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col md:gap-0 md:mt-0 mt-20 gap-10 md:flex-row min-h-screen md:px-28 px-4 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-white to-white items-center justify-center md:justify-between">
-        <div className="flex flex-col md:items-start md:justify-start items-center justify-center">
+      <main 
+        style={{
+          backgroundImage: 'url("/medicine.jpg")',
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          textAlign: 'center'
+          
+        }}
+        className="flex flex-col md:gap-0 md:mt-0 mt-20 gap-10 md:flex-row min-h-screen md:px-28 px-4 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-white to-white items-center justify-center md:justify-between">
+        <div 
+           
+          role="img"
+          aria-label="Map Image" 
+          className="flex flex-col md:items-start md:justify-start items-center justify-center">
           <section className="flex items-center gap-1">
             <AnimatedTextCharacter text="COM DOCTOR" />
             <span className="mb-5 font-semibold text-destructive uppercase text-sm rounded-lg">
@@ -36,13 +48,13 @@ export default function Home() {
             </Link>
           </section>
         </div>
-        <Image
+        {/* <Image
           src="/com-doctor-banner.jpeg"
           alt="hero banner"
           className="md:w-3/5 shadow-lg shadow-primary/20 w-full rounded-sm"
           width={1024}
           height={1024}
-        />
+        /> */}
       </main>
       <div className="md:px-24 px-4">
         <HowitWorks />
